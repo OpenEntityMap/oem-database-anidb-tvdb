aa58bf94afbe483c7f9d29c7c39975f9074c6434
Dean Gardiner <me@dgardiner.net>
2016-05-11 00:23:10 +1200

    Updated package name defined in [setup.py]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    name="oem-anidb-tvdb",
+    name="oem-database-anidb-tvdb",
     version="1.16.0",
     author="Dean Gardiner",
     author_email="me@dgardiner.net"
