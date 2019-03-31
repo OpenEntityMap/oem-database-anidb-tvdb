b58dd333a75d8eeb92bc075c18a6501abc932847
fm-build <fuzeman91+fm-build@gmail.com>
2019-04-01 02:28:32 +1300

    Bumped version to 1.19.13

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ PACKAGE_NAME = 'oem-database-anidb-tvdb'
 def build_config():
     config = {
         'name': PACKAGE_NAME,
-        'version': '1.19.12',
+        'version': '1.19.13',
 
         'author': 'Dean Gardiner',
         'author_email': 'me@dgardiner.net',
